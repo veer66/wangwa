8a3e89a998df68608546fad01352c424812e1dca
Vee Satayamas <vsatayamas@gmail.com>
2022-10-01 23:43:14 +0700

    feat: add alist update

--- alist.lisp
+++ alist.lisp
@@ -1,10 +1,20 @@
 (in-package :wangwa)
 
 (defun assoc* (m k v &key test)
-  (acons k
-	 v
-	 (remove-if (lambda (i)
-		      (if test
-			  (funcall test (car i) k)
-			  (eq (car i) k)))
-		    m)))
+  (loop for i in m
+	collect
+	(cond
+	  ((and test (funcall test (car i) k)) #1=(cons k v))
+	  ((and (not test) (eq (car i) k)) #1#)
+	  (t i))))
+
+(defun update (m k f &key test)
+  (loop for i in m
+	collect
+	(cond
+	  ((and test (funcall test (car i) k)) #1=(funcall f i))
+	  ((and (not test) (eq (car i) k)) #1#)
+	  (t i))))
+
+;; (update '((:X . 10) (:Y . 20)) :Y (lambda (i) (cons (car i) (+ 30 (cdr i)))))
+
